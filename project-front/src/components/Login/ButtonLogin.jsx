c498c2ca314023e668f47572c5a53a51495c24a9
Spunei Artur <arthurspunei@gmail.com>
2024-01-05 21:21:07 +0200

    Add ButtonLogin component and import it in LoginPage

---
+++ project-front/src/components/Login/ButtonLogin.jsx
@@ -0,0 +1,6 @@
+import './ButtonLogin.css';
+const ButtonLogin = ({ valoare }) => {
+  return <button className="button_for_login">{valoare}</button>;
+};
+
+export default ButtonLogin;
--- project-front/src/pages/LoginPage.jsx
+++ project-front/src/pages/LoginPage.jsx
@@ -1,6 +1,10 @@
+import ButtonLogin from '../components/Login/ButtonLogin';
+
 const Page = () => {
+  const result = 5 + 2;
   return (
     <div>
+      <ButtonLogin valoare="Login" />
       <h1>Login</h1>
     </div>
   );
